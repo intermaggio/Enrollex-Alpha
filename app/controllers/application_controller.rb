7deac1f57ea3b282f99df655f21c2c8aa21bb8e4
Chris Bolton <saiko_chriskun@shortmail.me>
2012-02-05 02:42:10 -0800

    minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :login
+  before_filter :auth
 
-  def login
+  def auth
     auto_login(User.find(cookies[:cm_user_id])) if cookies[:cm_user_id]
   end
 

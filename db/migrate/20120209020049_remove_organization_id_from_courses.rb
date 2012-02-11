8bdc5d2600aa7cab4ca5bd3ecd012fafaa3e6dfd
Chris Bolton <saiko_chriskun@shortmail.me>
2012-02-10 17:30:54 -0800

    another one >_>

--- db/migrate/20120209020049_remove_organization_id_from_courses.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOrganizationIdFromCourses < ActiveRecord::Migration
-  def up
-    remove_column :courses, :organization_id
-      end
-
-  def down
-    add_column :courses, :organization_id, :integer
-  end
-end

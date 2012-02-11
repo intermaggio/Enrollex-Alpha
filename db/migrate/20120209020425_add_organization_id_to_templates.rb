5c5770ee1b31bb11a8f23020a67ec9cd8a45e343
Chris Bolton <saiko_chriskun@shortmail.me>
2012-02-10 17:27:46 -0800

    moar bad migrations

--- db/migrate/20120209020425_add_organization_id_to_templates.rb
+++
@@ -1,6 +0,0 @@
-class AddOrganizationIdToTemplates < ActiveRecord::Migration
-  def change
-    add_column :templates, :organization_id, :integer
-
-  end
-end
--- db/migrate/20120209033139_remove_template_id_from_courses.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTemplateIdFromCourses < ActiveRecord::Migration
-  def up
-    remove_column :courses, :template_id
-      end
-
-  def down
-    add_column :courses, :template_id, :integer
-  end
-end

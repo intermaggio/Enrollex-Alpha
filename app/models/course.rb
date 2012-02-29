ec7a71b01779074b6966f1ca7ce238fc2e317324
Chris Bolton <saiko_chriskun@shortmail.me>
2012-02-28 21:35:39 -0800

    minor fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -24,6 +24,7 @@ class Course < ActiveRecord::Base
   before_save do
     self.lowname = self.name.downcase.gsub(' ', '_').gsub(/\W/, '')
     self.published_at = Time.now.to_date if self.published_at.nil? && self.published
-    self.start_date = self.days.reorder(:date).first.date if self.days.first
+    days = self.days.reorder(:date)
+    self.start_date = days.first.date if days.first
   end
 end

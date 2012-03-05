db2f42ed5a1a41eb8350fb0868729a6b4da5ed71
Chris Bolton <saiko_chriskun@shortmail.me>
2012-03-04 20:39:34 -0800

    catalog search functions fixed up

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,33 +1,40 @@
 class SiteController < ApplicationController
 
   def search
+    @html, page = fetch_courses(params[:uid], params[:mirai], params[:q])
+    if page == :enrollments
+      respond_to do |format|
+        format.js { render 'search_enrollments' }
+      end
+    end
+  end
+
+  def fetch_courses(uid, mirai, query)
     begin
-      user = User.find params[:uid]
-      @html = {}
+      user = User.find uid
+      html = {}
       courses = user.courses.where(organization_id: organization.id)
       if courses.first
-        courses = courses.mirai if params[:mirai] == 'true'
-        courses = courses.search(params[:q]).reorder(:created_at)
-        @html[user.id] = render_to_string courses
+        courses = courses.mirai if mirai == 'true'
+        courses = courses.search(query).reorder(:created_at)
+        html[user.id] = render_to_string courses
       end
       user.campers.each do |camper|
         courses = camper.courses.where(organization_id: organization.id)
         if courses.first
-          courses = courses.mirai if params[:mirai] == 'true'
-          courses = courses.search(params[:q]).reorder(:created_at)
-          @html[camper.id] = render_to_string courses
+          courses = courses.mirai if mirai == 'true'
+          courses = courses.search(query).reorder(:created_at)
+          html[camper.id] = render_to_string courses
         end
       end
-      respond_to do |format|
-        format.js { render 'search_enrollments' }
-      end
+      return html, :enrollments
     rescue
       courses = organization.courses
-      courses = courses.mirai if params[:mirai] == 'true'
-      courses = courses.search(params[:q]).reorder(:created_at)
+      courses = courses.mirai if mirai == 'true'
+      courses = courses.search(query).reorder(:created_at)
       courses = courses.published if !current_user || current_user && !current_user.admin_organizations.include?(organization)
-      @html = render_to_string courses
-      respond_to :js
+      html = render_to_string courses
+      return html, :all
     end
   end
 

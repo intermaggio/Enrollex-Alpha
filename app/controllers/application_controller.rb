ff05a6448f45af1276c1ae2ec8018b5773de6038
Chris Bolton <saiko_chriskun@shortmail.me>
2012-04-03 20:22:39 -0700

    minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < ActionController::Base
   end
 
   def age(dob)
-    now = Time.now.utc.to_date
+    now = Time.now.utc
     now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
   end
 

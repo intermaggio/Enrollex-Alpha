9a92d89007a95c653a91fdf1f2a646f9132e69a5
Chris Bolton <c@chrisbolton.me>
2012-04-28 18:13:58 -0400

    monthly charging complete

--- clock.rb
+++ clock.rb
@@ -6,13 +6,13 @@ handler do |job|
     when 'organization.charge'
       if Time.now.utc.day == 1 || Rails.env != 'production'
         Organization.all.each do |organization|
-          if !organization.last_charge || organization.last_charge < Time.now.utc.month - 1
+          if !organization.last_charge || organization.last_charge < Time.now.utc.month - 1 || Rails.env != 'production'
             Stripe.api_key = organization.stripe_secret
-            end_date = Time.now.utc - Time.now.utc.mday.days
-            start_date = end_date - 1.month + 1.day
-            transactions = CampersCourses.where(org_id: organization.id).within(start_date, end_date).map{|c| Stripe::Charge.retrieve(c.stripe_id).amount}
+            end_date = Time.now.utc - Time.now.utc.mday.days + 1.month
+            start_date = end_date - end_date.mday.days + 1.day
+            transactions = CampersCourses.where(org_id: organization.id).within(start_date, end_date).map{|c| c.stripe_id}.uniq
             if transactions.present?
-              amount = transactions.reduce(:+) * 0.021
+              amount = transactions.map{|id| Stripe::Charge.retrieve(id).amount}.reduce(:+) * 0.021
               Stripe.api_key =
                 if Rails.env == 'production'
                   'XfaZC4N7Fprblt21L8o91wFmsr0iGnYR'
@@ -20,12 +20,12 @@ handler do |job|
                   's6f5O2kuPgMtxRDwA2cZ4RmPhCd8a4rX'
                 end
               stripe = Stripe::Charge.create(
-                amount: amount.to_i,
+                amount: amount.round,
                 currency: 'usd',
                 customer: organization.card,
-                description: "#{organization.name} for #{(Time.now.utc - 1.day).strftime("%B")}"
+                description: "#{organization.name} for #{start_date.strftime("%B")}"
               )
-              organization.update_attribute(:last_charge, (Time.now.utc - 1.day).month)
+              organization.update_attribute(:last_charge, start_date.month)
             end
           end
         end

4abfe6f4348cc49b1814f057f7c63abf6342bc89
Chris Bolton <c@chrisbolton.me>
2014-05-04 17:49:08 -0700

    move api key to env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'Rackspace',
     rackspace_username: 'stythys',
-    rackspace_api_key: 'a3fa42b5b85d9f69dc1c3c0f4bf28901'
+    rackspace_api_key: ENV['RACKSPACE_KEY']
   }
   config.fog_directory = 'CourseManage'
   config.fog_host = 'http://c4242148.r48.cf2.rackcdn.com'
